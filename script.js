1936ee04be585a0e3336a50c0d6bbe98b67fe6df
아시 あし <alyagsosiji@gmail.com>
2026-09-13 19:16:38 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
    ========================================================== */
 const TARGET_YEAR  = 2026;  // 연도
 const TARGET_MONTH = 9;     // 월 (1 ~ 12)
-const TARGET_DAY   = 5;     // 일 (1 ~ 31)
-const TARGET_HOUR  = 10;     // 시 (0 ~ 23)
-const TARGET_MIN   = 35;     // 분 (0 ~ 59)
+const TARGET_DAY   = 19;     // 일 (1 ~ 31)
+const TARGET_HOUR  = 8;     // 시 (0 ~ 23)
+const TARGET_MIN   = 0;     // 분 (0 ~ 59)
 const TARGET_SEC   = 0;     // 초 (0 ~ 59)
 /* ========================================================== */
 
